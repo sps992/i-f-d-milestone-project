2ff443c35ff3ce10784a8d7fe1e24a1bff6925b2
Sam sedgeman <hello@samsedgeman.co.uk>
2019-07-21 12:29:14 +0000

    Adding initial graphs as a starting point for project. Need to adjust some css to improve user experience.

--- static/js/graph.js
+++ static/js/graph.js
@@ -6,7 +6,8 @@ queue()
 function makeGraphs(error, characterData) {
     var ndx = crossfilter(characterData);
     show_pieGenderSelector(ndx)
-   
+    show_death_stats(ndx);
+    show_group_by_surname(ndx);
     
     dc.renderAll();
 }
@@ -17,12 +18,44 @@ function show_pieGenderSelector(ndx){
     group = dim.group()
     
     dc.pieChart("#pieGenderSelector")
-    .width(800)
+    .width(250)
     .height(300)
     .dimension(dim)
     .group(group)
     .transitionDuration(500)
     
-    
 }
 
+
+
+function show_death_stats(ndx) {
+    var dim = ndx.dimension(dc.pluck('Alive?'));
+    var group = dim.group();
+    
+    dc.barChart("#deathTally")
+        .width(300)
+        .height(300)
+        .x(d3.scale.ordinal())
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .xUnits(dc.units.ordinal)
+        .elasticY(false)
+        .xAxisLabel("Mortality")
+        .yAxisLabel("Amount of People")
+        .yAxis().ticks(20);
+        
+        
+}
+
+function show_group_by_surname(ndx) {
+    var dim = ndx.dimension(dc.pluck('Surname'));
+    var group = dim.group();
+    
+    dc.selectMenu("#houseMenu")
+        .dimension(dim)
+        .group(group);
+        
+        
+}
